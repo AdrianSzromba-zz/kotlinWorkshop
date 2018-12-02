fcfa715bd8aa6fae2da44d653d9a364638867e87
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-02 18:56:42 +0100

    test

--- src/test/kotlin/blackjack/CardTest.kt
+++ src/test/kotlin/blackjack/CardTest.kt
@@ -6,14 +6,20 @@ import kotlin.test.assertEquals
 class CardTest {
 
     @Test
-    fun t1 () {
-        val card1 = Card(1,1)
+    fun t1() {
+        /*
+        val card1 = Card(1,1) name parameters and change order like below
         val card2 = Card(13,4)
+        */
+        val card1 = Card(value = 1, suit = 1)
+        val card2 = Card(value = 13, suit = 4)
 
         assertEquals(1, card1.value)
         assertEquals(1, card1.suit)
+        assertEquals("Spades", card1.suitName)
 
         assertEquals(13, card2.value)
         assertEquals(4, card2.suit)
+        assertEquals("Diamonds", card2.suitName)
     }
 }
