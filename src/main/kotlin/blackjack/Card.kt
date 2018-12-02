e58197da34cdef2b28db122de0f5d9608134e32e
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-02 19:23:52 +0100

    getter with "when"

--- src/main/kotlin/blackjack/Card.kt
+++ src/main/kotlin/blackjack/Card.kt
@@ -1,10 +1,21 @@
 package blackjack
 
+import java.lang.IllegalStateException
+
 class Card(val value: Int, val suit: Int) {
 
-    val suitName: String get() = "Spades" //Shortcut to create getter if there is onle line return statement
+    //    val suitName: String get() = "Spades" //Shortcut to create getter if there is onle line return statement
+    val suitName: String
+        get() = when (suit) { //works like switch in java
+            1 -> "Spades"
+            2 -> "Hearts"
+            3 -> "Clubs"
+            4 -> "Diamonds"
+            else -> throw IllegalStateException
+        }
 
-    val valueName: String get() {
+    val valueName: String
+        get() {
             return "Ace"
         }
 }
