ff69a5a2bb0689e70fc414fc5e942f34408ad959
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-12 12:00:15 +0100

    trying new functions with lambdas

--- src/test/kotlin/testCode/LambdaTest.kt
+++ src/test/kotlin/testCode/LambdaTest.kt
@@ -7,27 +7,29 @@ class LambdaTest {
     @Test
     fun testAnonymousFunction() {
 
-        val fun1 = fun() { // assign anonymous fun to variable
+        // later, We can pass this variable to other functions
+        val fun1 = fun() { // assign anonymous fun to variable (anonymous because don't have a name)
             print(123)
         }
-
 
 //        val fun2 = fun(p1: Int): Int {
 //            return p1 * p1
 //        }
         val fun2 = fun(p1: Int) = p1 * p1 //One state fun can be shorter
 
-
-        fun1()
-        println(fun2(1))
-
-        useFunctions(fun1)
         useFunctions2(fun1, fun2)
+        useFunctions2(fun() {print(123)}, fun(p1) = p1 * p1)
     }
 
     @Test
     fun testLambdaFunction() {
+        val fun1 = { print(123) } //like anonymous fun, but delete word fun() if no args
 
+        val fun2 = { p1:Int -> p1 * p1 }
+
+        useFunctions2(fun1, fun2)
+        useFunctions2({print(123)}, { p1:Int -> p1 * p1 }) //if there is one argument, can replace p1 by word 'it'
+        useFunctions2({print(123)}, { it * it }) // and skip first part
     }
 
     fun useFunctions(f1: () -> Unit) { // takes one argument of type: function
