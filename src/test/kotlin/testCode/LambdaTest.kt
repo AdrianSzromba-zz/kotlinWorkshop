0a1b29e2fec54f15ce15f3f3de669b963d8bbe28
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-12 11:30:27 +0100

    test

--- src/test/kotlin/testCode/LambdaTest.kt
+++ src/test/kotlin/testCode/LambdaTest.kt
@@ -11,14 +11,18 @@ class LambdaTest {
             print(123)
         }
 
-        val fun2 = fun(p1: Int): Int {
-            return p1 * p1
-        }
+
+//        val fun2 = fun(p1: Int): Int {
+//            return p1 * p1
+//        }
+        val fun2 = fun(p1: Int) = p1 * p1 //One state fun can be shorter
+
 
         fun1()
         println(fun2(1))
 
         useFunctions(fun1)
+        useFunctions2(fun1, fun2)
     }
 
     @Test
@@ -28,5 +32,10 @@ class LambdaTest {
 
     fun useFunctions(f1: () -> Unit) { // takes one argument of type: function
         f1() // call function that is passed as arg
+    }
+
+    fun useFunctions2(f1: () -> Unit, f2: (Int) -> Int) {
+        f1()
+        println(f2(10))
     }
 }
