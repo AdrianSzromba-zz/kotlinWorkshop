6c25a6f85d89b44591244326d26659ca9c6a0651
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-17 21:35:14 +0100

    create 2 players

--- src/test/kotlin/blackjack/HandTest.kt
+++ src/test/kotlin/blackjack/HandTest.kt
@@ -1,2 +1,18 @@
 package blackjack
 
+import org.junit.Test
+
+class HandTest {
+
+    @Test
+    fun t1() {
+        val h1 = Hand("Player")
+        h1.add(Card(1,1))
+        h1.add(Card(13,4))
+
+        val h2 = Hand("Player")
+        h2.add(Card(1,1))
+        h2.add(Card(2,1))
+        h2.add(Card(3,1))
+    }
+}
