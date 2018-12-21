94442a9c30f209d359e1639e78cd7616b9c712e5
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-21 18:25:11 +0100

    size and points fields added

--- src/main/kotlin/blackjack/Hand.kt
+++ src/main/kotlin/blackjack/Hand.kt
@@ -3,6 +3,15 @@ package blackjack
 class Hand(val name: String) {
 
     private val cards = mutableListOf<Card>() // in this class 'name' have default public accesor
+    val size get() = cards.size
+    val points: Int
+        get() {
+            var t = 0
+            for (c in cards) {
+                t += c.points
+            }
+            return t
+        }
 
     fun add(card: Card) {
         cards.add(card)
