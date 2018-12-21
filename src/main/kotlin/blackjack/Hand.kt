722e6429dc943d60aa27cdd15948eef6837a4f32
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-21 18:16:24 +0100

    implement add method

--- src/main/kotlin/blackjack/Hand.kt
+++ src/main/kotlin/blackjack/Hand.kt
@@ -2,4 +2,10 @@ package blackjack
 
 class Hand(val name: String) {
 
+    private val cards = mutableListOf<Card>() // in this class 'name' have default public accesor
+
+    fun add(card: Card) {
+        cards.add(card)
+    }
+
 }
