061eb5cce977e42a9e613543052f37fdd52251ed
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-21 18:45:23 +0100

    use funcinal programming

--- src/main/kotlin/blackjack/Hand.kt
+++ src/main/kotlin/blackjack/Hand.kt
@@ -4,12 +4,13 @@ class Hand(val name: String) {
 
     private val cards = mutableListOf<Card>() // in this class 'name' have default public accesor
     val size get() = cards.size
-    val points: Int
-        get() {
-            var t = 0
-            for (c in cards) t += c.points
-            return t
-        }
+    val points get() = cards.sumBy { it.points }
+//    val points1: Int
+//        get() {
+//            var t = 0
+//            for (c in cards) t += c.points
+//            return t
+//        }
 //    second method
 //    val points: Int
 //        get() {
