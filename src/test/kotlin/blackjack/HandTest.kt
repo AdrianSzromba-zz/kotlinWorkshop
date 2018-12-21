ccf8e94237d3676af8055c98cabfdef91dc58cac
AdrianSzromba <adrian.szromba@gmail.com>
2018-12-21 18:07:01 +0100

    test Dealer Hand

--- src/test/kotlin/blackjack/HandTest.kt
+++ src/test/kotlin/blackjack/HandTest.kt
@@ -1,6 +1,7 @@
 package blackjack
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class HandTest {
 
@@ -10,9 +11,17 @@ class HandTest {
         h1.add(Card(1,1))
         h1.add(Card(13,4))
 
-        val h2 = Hand("Player")
+        val h2 = Hand("Dealer")
         h2.add(Card(1,1))
         h2.add(Card(2,1))
         h2.add(Card(3,1))
+
+        assertEquals("Player", h1.name)
+        assertEquals(11, h1.points)
+        assertEquals(2, h1.size)
+
+        assertEquals("Dealer", h1.name)
+        assertEquals(6, h2.points)
+        assertEquals(3, h2.size)
     }
 }
